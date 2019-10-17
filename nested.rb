c4c22810a23df1d7bdd50b1bc4a7972db9b50da5
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-17 02:35:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,10 +17,14 @@ def hopper
      }
 
 
+
+return programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	
 	programmer_hash = 
  		{
@@ -37,6 +41,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +61,12 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash = 
  		{
@@ -79,7 +83,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:yukihiro_matsumoto] = {
+       :known_for => "ruby",
+       :languages => "C"
+     }
 
+return programmer_hash[:yukihiro_matsumoto][:languages]
     
 end
 
@@ -102,6 +112,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
